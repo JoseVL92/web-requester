c20e91afde73605728eb7bb34a887f790a9be116
Jose Alberto <jovalab92@gmail.com>
2019-11-08 11:48:13 -0500

    Updating version and classifiers

--- setup.py
+++ setup.py
@@ -2,14 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="http_requests",
-    version=1.0,
+    version=0.1,
     author="Jose Alberto Varona Labrada",
     author_email="jovalab92@gmail.com",
     description="HTTP sync / async python (>=3.6) library that works with requests and aiohttp libraries, exploiting the best of each one",
     python_requires=">=3.6",
-    project_urls={
-        "Source Code": "https://github.com/JoseVL92/http-requests"
-    },
     url="https://github.com/JoseVL92/http-requests",
     packages=find_packages(),
     package_data={
@@ -20,7 +17,12 @@ setup(
     },
     install_requires=['requests', 'aiohttp'],
     classifiers=[
-            'License :: MIT'
-        ],
-    license='MIT'
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    license='MIT',
+    keywords=['http', 'asynchronous', 'request']
 )
