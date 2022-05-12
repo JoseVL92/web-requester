909b5a776758d4a768e56dd2fbfdbcfaf7acc003
JoseVL92 <jovalab92@gmail.com>
2022-05-12 10:48:21 -0400

    Updating info

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
-# http_requests
-HTTP sync / async python library that works with both: requests and aiohttp, exploiting the best of each one
-
-##### NOTE
-If obj = aiohttp.ClientSession, then calling obj.loop is deprecated for reasons discussed in [this github issue](https://github.com/aio-libs/aiohttp/issues/3331), but this project use it for convenience
+# web-requester
+HTTP sync / async python library. Uses both requests and aiohttp.
 
--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="http_requests",
-    version=0.1,
+    name="web-requester",
+    version=0.2,
     author="Jose Alberto Varona Labrada",
     author_email="jovalab92@gmail.com",
-    description="HTTP sync / async python (>=3.6) library that works with requests and aiohttp libraries, exploiting the best of each one",
+    description="HTTP sync / async python (>=3.6) library that works with requests and aiohttp libraries",
     python_requires=">=3.6",
-    url="https://github.com/JoseVL92/http-requests",
-    download_url="https://github.com/JoseVL92/http-requests/archive/v_01.tar.gz",
+    url="https://github.com/JoseVL92/web-requester",
+    download_url="https://github.com/JoseVL92/web-requester/archive/v0.1.tar.gz",
     packages=find_packages(),
     data_files=[
         ("", ["LICENSE.txt", "README.md"])
     ],
-    install_requires=['requests', 'aiohttp'],
+    install_requires=['aiohttp', 'requests'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
     license='MIT',
     keywords=['http', 'asynchronous', 'request']
