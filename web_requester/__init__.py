fcd55e07e5b35db4c87c4e5c24e7debad1f99a53
JoseVL92 <jovalab92@gmail.com>
2022-05-19 12:33:29 -0400

    Removing get_valid_loop and AsyncResponse, erased since 0.3

--- web_requester/__init__.py
+++ web_requester/__init__.py
@@ -1,2 +1,2 @@
-from .requester import AsyncResponse, async_request, chain_callback, create_aioclient,\
-    get_valid_loop, request_all, request_all_async, sync_request, sync_to_async_request
+from .requester import async_request, chain_callback, create_aioclient, \
+    request_all, request_all_async, sync_request, sync_to_async_request
